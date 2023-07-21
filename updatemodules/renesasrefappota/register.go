47eae042a9a5880691974cea664eeb71df6c4c22
Minh Khoa. Tran <khoa.tran.xk@renesas.com>
2023-07-21 08:32:20 +0700

    update

--- updatemodules/renesasrefappota/register.go
+++ updatemodules/renesasrefappota/register.go
@@ -1,10 +1,11 @@
+
 package renesasrefappota
 
 import (
-	"github.com/aoscloud/aos_updatemanager/updatehandler"
-	"github.com/aoscloud/aos_updatemanager/updatemodules/partitions/rebooters/xenstorerebooter"
-	"github.com/aoscloud/aos_updatemanager/updatemodules/partitions/updatechecker/systemdchecker"
-	"github.com/khoatm98/demo-soafee-rcar-s4/updatemodules/renesasrefappota"
+    "encoding/json"
+    "github.com/aoscloud/aos_common/aoserrors"
+    "github.com/aoscloud/aos_updatemanager/updatehandler"
+    "github.com/aoscloud/aos_updatemanager/updatemodules/partitions/rebooters/xenstorerebooter"
 )
 
 /*******************************************************************************
@@ -12,24 +13,24 @@ import (
  ******************************************************************************/
 
 func init() {
-	updatehandler.RegisterPlugin("renesasrefappota"func(id string, configJSON json.RawMessage,
-		storage updatehandler.ModuleStorage,
-		) (module updatehandler.UpdateModule, err error) {
-			if len(configJSON) == 0 {
-				return nil, aoserrors.Errorf("config for %s module is required", id)
-			}
+    updatehandler.RegisterPlugin("renesasrefappota",func(id string, configJSON json.RawMessage,
+   	 storage updatehandler.ModuleStorage,
+   	 ) (module updatehandler.UpdateModule, err error) {
+   		 if len(configJSON) == 0 {
+   			 return nil, aoserrors.Errorf("config for %s module is required", id)
+   		 }
 
-			var config moduleConfig
+   		 var config moduleConfig
 
-			if err = json.Unmarshal(configJSON, &config); err != nil {
-				return nil, aoserrors.Wrap(err)
-			}
+   		 if err = json.Unmarshal(configJSON, &config); err != nil {
+   			 return nil, aoserrors.Wrap(err)
+   		 }
 
-			if module, err = renesasrefappota.New(id, config.VersionFile, config.UpdateDir,
-				storage, &xenstorerebooter.XenstoreRebooter{}, systemdchecker.New(config.SystemdChecker)); err != nil {
-				return nil, aoserrors.Wrap(err)
-			}
+   		 if module, err = New(id, configJSON,
+   			 storage, &xenstorerebooter.XenstoreRebooter{}); err != nil {
+   			 return nil, aoserrors.Wrap(err)
+   		 }
 
-			return module, nil
-		})
+   		 return module, nil
+   	 })
 }
